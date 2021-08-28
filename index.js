9ec5dfbc9f7d6baa9e8ca7c3f2f06d04d5c6b12d
Ilya Pichugin <ilia-pichugin2014@yandex.ru>
2021-08-28 15:00:12 +0300

    slider28.08

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 document.addEventListener( 'DOMContentLoaded', function () {
 	new Splide( '#splide', {
-        type   : 'loop',
         focus    : 'center',
         perPage  : 2,
         trimSpace: false,
@@ -8,8 +7,10 @@ document.addEventListener( 'DOMContentLoaded', function () {
         classes: {
             arrows: 'splide__arrows your-class-arrows',
             arrow : 'splide__arrow your-class-arrow',
-            prev  : 'splide__arrow--prev your-class-prev',
-            next  : 'splide__arrow--next your-class-next',
+            prev  : 'slider__button_direction_left',
+            next  : 'slider__button_direction_right',
+            pagination : 'slider',
+            page      : 'slider__dot-button',
         },
     } ).mount();
 } );
