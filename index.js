c823b7d0b0c54c131dde542e30d4679d53f2f05a
Ilya Pichugin <ilia-pichugin2014@yandex.ru>
2021-09-02 08:00:27 +0300

    Доделан второй слайдер

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ document.addEventListener( 'DOMContentLoaded', function () {
         },
     } ).mount();
 } );
+
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#splide2', {
+    perPage  : 3,
+    arrows: true,
+    pagination: 'slider',
+    classes: {
+        arrows: 'splide__arrows your-class-arrows',
+        arrow : 'splide__arrow your-class-arrow',
+        prev  : 'slider__button_direction_left',
+        next  : 'slider__button_direction_right',
+        pagination : 'slider__pagination',
+        page      : 'slider__dot-button',
+    },
+} ).mount();
+} );
